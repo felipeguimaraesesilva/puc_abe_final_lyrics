0583fbe50d12f77b61aa491bc2fe835c4e5bd961
felipeguimaraesesilva <felipeguimaraesesilva@gmail.com>
2019-06-09 14:53:33 -0300

    Versionamento

--- puc_abe_lyrics_api/src/main/java/com/guimaraes/controller/LyricsController.java
+++ puc_abe_lyrics_api/src/main/java/com/guimaraes/controller/LyricsController.java
@@ -3,6 +3,9 @@ package com.guimaraes.controller;
 import org.jmusixmatch.MusixMatch;
 import org.jmusixmatch.MusixMatchException;
 import org.jmusixmatch.entity.lyrics.Lyrics;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.guimaraes.constant.AppConstants;
+import com.netflix.discovery.EurekaClient;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
@@ -17,6 +21,13 @@ public class LyricsController {
 
 	private static final String RESOURCE_NAME = "/lyric";
 	private static final String CONTEXT_RESOURCE = "/" + AppConstants.VERSION_V1 + RESOURCE_NAME;
+
+	@Autowired
+	@Lazy
+	private EurekaClient eurekaClient;
+
+	@Value("${spring.application.name}")
+	private String appName;
 
 	@HystrixCommand(fallbackMethod = "reliable")
 	@GetMapping(CONTEXT_RESOURCE)
